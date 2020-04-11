73ea1b3de868e9ab05f84b988c234ffc67839094
Alngo <alngo@student.42.fr>
2020-04-11 16:22:00 +0200

    feat(math): add avg rate of return.

--- tests/context.py
+++ tests/context.py
@@ -7,4 +7,4 @@ sys.path.insert(0, os.path.abspath(root_path))
 from tim22.tim22 import Tim22  # noqa
 from tim22.database.DBMarket import DBMarket  # noqa
 from tim22.database.marketstore.DBMarketStore import DBMarketStore  # noqa
-from tim22.maths.rateOfReturn import simple_rate_of_return
+from tim22.maths.rateOfReturn import simple_rate_of_return, avg_daily_rate_of_return, avg_annual_rate_of_return
--- tests/maths/rateOfReturn_test.py
+++ tests/maths/rateOfReturn_test.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 import pandas as pd
-from tests.context import simple_rate_of_return
+from tests.context import simple_rate_of_return, avg_daily_rate_of_return, avg_annual_rate_of_return
 
 def test_simple_rate_of_return():
     df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10], columns=["Close"])
@@ -12,3 +12,13 @@ def test_simple_rate_of_return():
     assert round(expected[2], 2) == 0.50
     assert round(expected[3], 2) == 0.33
     assert round(expected[4], 2) == 0.25
+
+def test_avg_daily_rate_of_return():
+    df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10], columns=["Close"])
+    avg = avg_daily_rate_of_return(df)
+    assert type(avg) is np.float64
+
+def test_avg_annual_rate_of_return():
+    df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10], columns=["Close"])
+    avg = avg_annual_rate_of_return(df)
+    assert type(avg) is np.float64

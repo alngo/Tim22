838067cfc4f6e21dc351da3da1bba28a7ae5045e
Alngo <alngo@student.42.fr>
2020-04-11 15:57:22 +0200

    feat(math): remove print

--- tests/maths/rateOfReturn_test.py
+++ tests/maths/rateOfReturn_test.py
@@ -6,11 +6,9 @@ from tests.context import simple_rate_of_return
 def test_simple_rate_of_return():
     df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10], columns=["Close"])
     expected = simple_rate_of_return(df)
-    print(expected.head())
     assert type(expected) is pd.Series
     assert np.isnan(expected[0])
     assert round(expected[1], 2) == 1.00
     assert round(expected[2], 2) == 0.50
     assert round(expected[3], 2) == 0.33
     assert round(expected[4], 2) == 0.25
-
